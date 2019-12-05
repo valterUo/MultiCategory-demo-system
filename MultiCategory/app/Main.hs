56c937e0d4baa7f304828c4c71a1bd3cf88576c5
valterUo <valter.uotila@gmail.com>
2019-12-05 19:07:13 +0200

    NimbleGraph updates

--- MultiCategory/app/Main.hs
+++ MultiCategory/app/Main.hs
@@ -28,6 +28,10 @@ import Data.RDF as RDF
 import qualified Data.Text as T
 import Unibench.DataParser
 import Unibench.SchemaCategory
+import NimbleGraph.NimbleGraph
+import NimbleGraph.NimbleGraphToD3js
+import Unibench.DataState
+import qualified Data.HashMap.Strict as Map
 
 encodeListToJSON :: ToJSON a => [a] -> [B.ByteString]
 encodeListToJSON [] = []
@@ -37,9 +41,14 @@ wrapListToJSON :: ToJSON a => [a] -> String
 wrapListToJSON xs = "{\"result\":[" ++ L.unpack( L.intercalate (L.pack ", ") (encodeListToJSONText xs)) ++ "]}"
 
 main = do
-    print $ (decode(B.packChars "{\"orderid\": \"016f6a4a-ec18-4885-b1c7-9bf2306c76d6\", \"personid\": \"10995116278711\", \"orderdate\": \"2022-09-01\", \"totalprice\": 723.88, \"orderline\": [{\"productId\": \"6465\", \"asin\": \"B000FIE4WC\", \"title\": \"Topeak Dual Touch Bike Storage Stand\", \"price\": 199.95, \"brand\": \"MYLAPS_Sports_Timing\"}, {\"productId\": \"178\", \"asin\": \"B002Q6DB7A\", \"title\": \"Radians Eclipse RXT Photochromic Lens with Black Frame Glass\", \"price\": 61.99, \"brand\": \"Elfin_Sports_Cars\"}, {\"productId\": \"6427\", \"asin\": \"B000SE9LDK\", \"title\": \"Sportlock Leatherlock Series Deluxe Take-Down Shotgun Case\", \"price\": 84.99, \"brand\": \"MYLAPS_Sports_Timing\"}, {\"productId\": \"7570\", \"asin\": \"B005G2G2OU\", \"title\": \"ESEE-5 Serr Olive Drab Textured Poweder Coated Blade Drop Point Style 1095 Carbon Steel-57 Rc\", \"price\": 172.95, \"brand\": \"Derbi\"}, {\"productId\": \"1991\", \"asin\": \"B00245TWWG\", \"title\": \"Marcy Classic MD 859P Mid Size Bench\", \"price\": 204.0, \"brand\": \"CCM_(ice_hockey)\"}]}") :: Maybe UnibenchOrder)
-    result <- collectUnibenchOrders "D:\\Unibench-0.1\\Dataset\\Order\\new_order.json"
-    print result
+    print $ encode $ createD3NimbleGraph $ foldNimble (\vertex newGraph -> let 
+        person = vertexValue vertex in 
+            if firstName person == "Li" && lastName person == "Li" 
+                then addVertex vertex newGraph else newGraph) (\edge newGraph -> case (Map.lookup (vertexId $ NimbleGraph.NimbleGraph.source edge) (NimbleGraph.NimbleGraph.vertices newGraph)) of Nothing -> newGraph; Just(sourceVertex) -> case Map.lookup (vertexId $ NimbleGraph.NimbleGraph.target edge) (NimbleGraph.NimbleGraph.vertices newGraph) of Nothing -> newGraph; Just(targetVertex) -> addEdge edge newGraph) emptyNimbleGraph personKnowsPersonGraph
+
+    -- print $ (decode(B.packChars "{\"orderid\": \"016f6a4a-ec18-4885-b1c7-9bf2306c76d6\", \"personid\": \"10995116278711\", \"orderdate\": \"2022-09-01\", \"totalprice\": 723.88, \"orderline\": [{\"productId\": \"6465\", \"asin\": \"B000FIE4WC\", \"title\": \"Topeak Dual Touch Bike Storage Stand\", \"price\": 199.95, \"brand\": \"MYLAPS_Sports_Timing\"}, {\"productId\": \"178\", \"asin\": \"B002Q6DB7A\", \"title\": \"Radians Eclipse RXT Photochromic Lens with Black Frame Glass\", \"price\": 61.99, \"brand\": \"Elfin_Sports_Cars\"}, {\"productId\": \"6427\", \"asin\": \"B000SE9LDK\", \"title\": \"Sportlock Leatherlock Series Deluxe Take-Down Shotgun Case\", \"price\": 84.99, \"brand\": \"MYLAPS_Sports_Timing\"}, {\"productId\": \"7570\", \"asin\": \"B005G2G2OU\", \"title\": \"ESEE-5 Serr Olive Drab Textured Poweder Coated Blade Drop Point Style 1095 Carbon Steel-57 Rc\", \"price\": 172.95, \"brand\": \"Derbi\"}, {\"productId\": \"1991\", \"asin\": \"B00245TWWG\", \"title\": \"Marcy Classic MD 859P Mid Size Bench\", \"price\": 204.0, \"brand\": \"CCM_(ice_hockey)\"}]}") :: Maybe UnibenchOrder)
+    -- result <- collectUnibenchOrders "D:\\Unibench-0.1\\Dataset\\Order\\new_order.json"
+    -- print result
 
     -- result <- parseFile NTriplesParser "D:\\Helsinki Multi-Model Datasets\\Person_dataset\\Person_dataset\\reprocessed_dataset\\small_new_personGraph.graph"
     -- case result of
--- MultiCategory/src/NimbleGraph/NimbleGraph.hs
+++ MultiCategory/src/NimbleGraph/NimbleGraph.hs
@@ -4,7 +4,7 @@ module NimbleGraph.NimbleGraph where
 
 import GHC.Generics
 import Data.Aeson
-import Data.HashMap.Strict
+import qualified Data.HashMap.Strict as Map
 import Data.Hashable
 import qualified Data.SortedList as SortList
 
@@ -24,46 +24,46 @@ data NimbleVertex a = NimbleVertex {
 } deriving (Show, Eq, Generic)
 
 data NimbleGraph a b = NimbleGraph {
-    vertices :: HashMap String (NimbleVertex a),
-    edges :: HashMap String (NimbleEdge a b)
+    vertices :: Map.HashMap String (NimbleVertex a),
+    edges :: Map.HashMap String (NimbleEdge a b)
 } deriving (Show, Eq, Generic)
 
 -- NimbleGraph creation
 
 emptyNimbleGraph :: NimbleGraph a b
-emptyNimbleGraph = NimbleGraph empty empty
+emptyNimbleGraph = NimbleGraph Map.empty Map.empty
 
 singleNimbleGraph :: Hashable a => a -> b -> NimbleGraph a b
 singleNimbleGraph vertex edge = addVertex (NimbleVertex (show(hash vertex)) vertex (SortList.toSortedList []) (SortList.toSortedList [])) emptyNimbleGraph
 
 addEdge :: NimbleEdge a b -> NimbleGraph a b -> NimbleGraph a b
-addEdge edge graph = NimbleGraph (vertices graph) (insert (edgeId edge) edge (edges graph))
+addEdge edge graph = NimbleGraph (vertices graph) (Map.insert (edgeId edge) edge (edges graph))
 
 addVertex :: NimbleVertex a -> NimbleGraph a b -> NimbleGraph a b
-addVertex vertex graph = NimbleGraph (insert (vertexId vertex) vertex (vertices graph)) (edges graph)
+addVertex vertex graph = NimbleGraph (Map.insert (vertexId vertex) vertex (vertices graph)) (edges graph)
 
 mkGraphFromTuples :: [(String, (String, a), (String, a), [String], b)] -> NimbleGraph a b
-mkGraphFromTuples [] = NimbleGraph empty empty
+mkGraphFromTuples [] = NimbleGraph Map.empty Map.empty
 mkGraphFromTuples (x:xs) = let (id, source, target, labels, value) = x in
     let tailGraph = mkGraphFromTuples xs in
-        if member id (edges $ tailGraph) 
+        if Map.member id (edges $ tailGraph) 
             then tailGraph 
             else let (sourceId, sourceValue) = source in
                 let (targetId, targetValue) = target in
-                    if member sourceId (vertices tailGraph) && member targetId (vertices tailGraph)
-                        then let oldSourceVertex = (vertices tailGraph) ! sourceId in 
+                    if Map.member sourceId (vertices tailGraph) && Map.member targetId (vertices tailGraph)
+                        then let oldSourceVertex = (vertices tailGraph) Map.! sourceId in 
                                 let newSourceVertex = NimbleVertex (vertexId oldSourceVertex) (vertexValue oldSourceVertex) (inComingEdges oldSourceVertex) (SortList.insert id (outGoingEdges oldSourceVertex)) in
-                                    let oldTargetVertex = (vertices tailGraph) ! targetId in 
+                                    let oldTargetVertex = (vertices tailGraph) Map.! targetId in 
                                         let newTargetVertex = NimbleVertex (vertexId oldTargetVertex) (vertexValue oldTargetVertex) (SortList.insert id (inComingEdges oldTargetVertex)) (outGoingEdges oldTargetVertex) in
                                             addEdge (NimbleEdge id value labels newSourceVertex newTargetVertex) (addVertex newTargetVertex (addVertex newSourceVertex tailGraph))
-                        else if member targetId (vertices tailGraph) 
-                            then let oldTargetVertex = (vertices tailGraph) ! targetId in 
+                        else if Map.member targetId (vertices tailGraph) 
+                            then let oldTargetVertex = (vertices tailGraph) Map.! targetId in 
                                     let newTargetVertex = NimbleVertex (vertexId oldTargetVertex) (vertexValue oldTargetVertex) (SortList.insert id (inComingEdges oldTargetVertex)) (outGoingEdges oldTargetVertex) in
                                         addEdge (NimbleEdge id value labels (NimbleVertex sourceId sourceValue (SortList.toSortedList []) (SortList.toSortedList [id])) newTargetVertex) 
                                             (addVertex (NimbleVertex sourceId sourceValue (SortList.toSortedList []) (SortList.toSortedList [id])) 
                                                 (addVertex newTargetVertex tailGraph))
-                            else if member sourceId (vertices tailGraph)
-                                then let oldSourceVertex = (vertices tailGraph) ! sourceId in 
+                            else if Map.member sourceId (vertices tailGraph)
+                                then let oldSourceVertex = (vertices tailGraph) Map.! sourceId in 
                                         let newSourceVertex = NimbleVertex (vertexId oldSourceVertex) (vertexValue oldSourceVertex) (inComingEdges oldSourceVertex) (SortList.insert id (outGoingEdges oldSourceVertex)) in
                                             addEdge (NimbleEdge id value labels newSourceVertex (NimbleVertex targetId targetValue (SortList.toSortedList [id]) (SortList.toSortedList []))) 
                                                 (addVertex (NimbleVertex targetId targetValue (SortList.toSortedList [id]) (SortList.toSortedList [])) 
@@ -75,23 +75,37 @@ mkGraphFromTuples (x:xs) = let (id, source, target, labels, value) = x in
 -- NimbleGraph querying
 
 isEmpty :: NimbleGraph a b -> Bool
-isEmpty graph = (Data.HashMap.Strict.null $ vertices graph) && (Data.HashMap.Strict.null $ edges graph)
+isEmpty graph = (Map.null $ vertices graph) && (Map.null $ edges graph)
 
 size :: NimbleGraph a b -> String
-size graph = "Vertices: " ++ (show $ Data.HashMap.Strict.size $ vertices graph) ++ " Edges: " ++ (show $ Data.HashMap.Strict.size $ edges graph)
+size graph = "Vertices: " ++ (show $ Map.size $ vertices graph) ++ " Edges: " ++ (show $ Map.size $ edges graph)
 
 lookupVertexById :: String -> NimbleGraph a b -> Maybe (NimbleVertex a)
-lookupVertexById key graph = Data.HashMap.Strict.lookup key (vertices graph)
+lookupVertexById key graph = Map.lookup key (vertices graph)
 
 lookupEdgeById :: String -> NimbleGraph a b -> Maybe (NimbleEdge a b)
-lookupEdgeById key graph = Data.HashMap.Strict.lookup key (edges graph)
+lookupEdgeById key graph = Map.lookup key (edges graph)
 
 foldNimble :: (NimbleVertex a -> c -> c) -> (NimbleEdge a b -> c -> c) -> c -> NimbleGraph a b -> c
 foldNimble vertexFunction edgeFunction structure graph = if isEmpty graph then structure else
-    let foldedVertices = Data.HashMap.Strict.foldr vertexFunction structure (vertices graph) in
-        Data.HashMap.Strict.foldr edgeFunction foldedVertices (edges graph)
+    let foldedVertices = Map.foldr vertexFunction structure (vertices graph) in
+        Map.foldr edgeFunction foldedVertices (edges graph)
+
+inComingNeighbors :: NimbleVertex a -> NimbleGraph a b -> NimbleGraph a b
+inComingNeighbors vertex graph = let newEdges = foldr (\edgeKey xs -> case Map.lookup edgeKey (edges graph) of
+                                                        Nothing -> xs
+                                                        Just(e) -> Map.insert edgeKey e xs) Map.empty (SortList.fromSortedList $ inComingEdges vertex) in
+                                    let newVertices = Map.foldr (\e xs -> Map.insert (vertexId $ source e) (source e) xs) (Map.singleton (vertexId vertex) vertex) newEdges in 
+                                        NimbleGraph newVertices newEdges
+
+outGoingNeighbors :: NimbleVertex a -> NimbleGraph a b -> NimbleGraph a b
+outGoingNeighbors vertex graph = let newEdges = foldr (\edgeKey xs -> case Map.lookup edgeKey (edges graph) of
+                                                        Nothing -> xs
+                                                        Just(e) -> Map.insert edgeKey e xs) Map.empty (SortList.fromSortedList $ outGoingEdges vertex) in
+                                    let newVertices = Map.foldr (\e xs -> Map.insert (vertexId $ target e) (target e) xs) (Map.singleton (vertexId vertex) vertex) newEdges in 
+                                        NimbleGraph newVertices newEdges
 
 -- NimbleGraph manipulation
 
 nimbleGraphUnion :: NimbleGraph a b -> NimbleGraph a b -> NimbleGraph a b
-nimbleGraphUnion graph1 graph2 = NimbleGraph (union (vertices graph1) (vertices graph2)) (union (edges graph1) (edges graph2))
+nimbleGraphUnion graph1 graph2 = NimbleGraph (Map.union (vertices graph1) (vertices graph2)) (Map.union (edges graph1) (edges graph2))
